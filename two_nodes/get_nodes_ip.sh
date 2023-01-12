4da00a1bee6c8b8f02c54323187e8bfb0c80f6b5
namysl <e.k.namysl@gmail.com>
2023-01-12 15:10:04 +0100

    added two nodes files

---
+++ two_nodes/get_nodes_ip.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+worker1=$(vagrant ssh-config worker1 | grep HostName | grep -Po '(\d{1,3}.){4}')
+worker2=$(vagrant ssh-config worker2 | grep HostName | grep -Po '(\d{1,3}.){4}')
+
+echo -e "$worker1 worker1\n$worker2 worker2"
