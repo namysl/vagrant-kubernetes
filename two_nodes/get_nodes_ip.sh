c5bd65a65829c241c2eff0b08976b47a209e1b4c
namysl <e.k.namysl@gmail.com>
2023-01-12 15:15:41 +0100

    added instruction to echo

--- two_nodes/get_nodes_ip.sh
+++ two_nodes/get_nodes_ip.sh
@@ -3,4 +3,5 @@
 worker1=$(vagrant ssh-config worker1 | grep HostName | grep -Po '(\d{1,3}.){4}')
 worker2=$(vagrant ssh-config worker2 | grep HostName | grep -Po '(\d{1,3}.){4}')
 
+echo "IPs below should be included in master's /etc/hosts"
 echo -e "$worker1 worker1\n$worker2 worker2"
